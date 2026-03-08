a8ae6c09af73ab1c9fa1ee535dab2974f6a466b5
Dimitri NGOHO <dimitri.ngoho@lmtgroup.com>
2026-03-08 14:29:31 +0100

    Update syn2

--- GH/settings.py
+++ GH/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 # Application definition
 
 ALLOWED_HOSTS = ['*']
